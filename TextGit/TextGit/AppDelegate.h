a349bd3a90bcfd4076321e1008988448bbd298c6
Skiessss <zf_skies@163.com>
2017-03-20 16:15:38 +0800

    创建了项目
     建立 了工程 ,创建了项目

---
+++ TextGit/TextGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextGit
+//
+//  Created by mac on 17/3/20.
+//  Copyright © 2017年 ZhiFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextGit/TextGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextGit
+//
+//  Created by mac on 17/3/20.
+//  Copyright © 2017年 ZhiFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
